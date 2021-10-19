2bae41564b0551dcafd9400bc227ecb3311fc6fc
melissecabral <melissecabral@gmail.com>
2021-10-18 21:46:32 -0300

    Fix formatting to build.

--- src/pages/api/[ids].ts
+++ src/pages/api/[ids].ts
@@ -1,24 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   try {
     const {
-      query: { ids },
+      query: { ids }
     } = req;
 
     // Validations.
     const isValid = Array(ids).map((id) => String(id).match(/^[0-9]+$/));
-    if (!isValid)
-      throw res
-        .status(500)
-        .json({ error: "The ids must be an array of numbers!" });
+    if (!isValid) throw res.status(500).json({ error: 'The ids must be an array of numbers!' });
 
     return res.status(200).json(ids);
   } catch (error) {
-    const err = error instanceof Error ? error : new Error("Internal error!");
+    const err = error instanceof Error ? error : new Error('Internal error!');
     return res.status(500).json(err.message);
   }
 };
